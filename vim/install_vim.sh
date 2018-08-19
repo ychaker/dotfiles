8cc875d9528bb6970597292517bbc7842c9ddcfa
Youssef Chaker <youssef.chaker@gmail.com>
2018-08-19 16:05:04 -0500

    Updates vim setup.

--- vim/install_vim.sh
+++ vim/install_vim.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DOTVIM="$HOME/.vim"
-JQUERY=12107
 
 if [ ! -e `which git` ]
 then
@@ -22,19 +21,22 @@ get_repo() {
     then
         echo "Pulling latest from $repo"
         cd $DOTVIM/bundle/$repo
-        git pull origin master
+        git submodule update --init
         cd ..
     else
         echo "Cloning repo for $repo"
-        git clone git://github.com/$gh_user/$repo.git
+        git submodule add git://github.com/$gh_user/$repo.git
     fi
 }
+
+echo "Installing Seeing is Believing"
+gem install seeing_is_believing
 
 echo "Creating .vim folders if necessary"
 mkdir -p $DOTVIM/{autoload,bundle}
 cd $DOTVIM/bundle/
 
-tpope_repos=(rails haml git cucumber fugitive surround unimpaired abolish repeat markdown endwise)
+tpope_repos=(rails git cucumber fugitive surround unimpaired abolish repeat markdown endwise)
 
 for r in ${tpope_repos[*]}; do
 	repo="vim-$r"
@@ -48,19 +50,13 @@ echo "Installing Syntastic"
 get_repo "scrooloose" "syntastic"
 
 echo "Installing NERDCommenter"
-get_repo "jc00ke" "nerdcommenter"
+get_repo "scrooloose" "nerdcommenter"
 
 echo "Installing snipMate"
 get_repo "msanders" "snipmate.vim"
 
 echo "Installing vim-ruby"
 get_repo "vim-ruby" "vim-ruby"
-
-echo "Installing vim-ruby-debugger"
-get_repo "astashov" "vim-ruby-debugger"
-
-echo "Installing mustache.vim"
-get_repo "jc00ke" "mustache.vim"
 
 echo "Installing taglist.vim"
 get_repo "jc00ke" "taglist.vim"
@@ -69,14 +65,16 @@ echo "Installing ack.vim"
 get_repo "mileszs" "ack.vim"
 
 echo "Installing supertab"
-get_repo "tsaleh" "vim-supertab"
+get_repo "ervandew" "supertab"
 
 echo "Installing vim-indent-object"
-get_repo "michaeljsmith", "vim-indent-object"
+get_repo "michaeljsmith" "vim-indent-object"
 
-echo "Installing jQuery"
-mkdir -p jquery/syntax
-wget -c -O jquery/syntax/jquery.vim http://www.vim.org/scripts/download_script.php?src_id=$JQUERY
+echo "Installing vim-indent-object"
+get_repo "t9md" "vim-ruby-xmpfilter"
+
+echo "Installing vim-indent-object"
+get_repo "hwartig" "vim-seeing-is-believing"
 
 cd $DOTVIM/autoload
 echo "Fetching latest pathogen.vim"
@@ -84,7 +82,7 @@ rm pathogen.vim
 wget http://github.com/tpope/vim-pathogen/raw/master/autoload/pathogen.vim
 
 echo "Checking to see if pathogen has already been added to .vimrc"
-pathogen_cmd="call pathogen#runtime_append_all_bundles()"
+pathogen_cmd="execute pathogen#infect()"
 contains=`grep "$pathogen_cmd" ~/.vimrc | wc -l`
 
 if [ $contains == 0 ]
@@ -94,23 +92,3 @@ then
 else
 	echo "It was already added. Good to go"
 fi
-
-echo "Installing Command T plugin"
-ct=`curl -s https://wincent.com/products/command-t | grep releases | head -1 | cut -d\" -f2`
-cd /tmp
-vba=$( echo "$ct" | ruby -ruri -e 'puts File.basename(gets.chomp)' )
-wget $ct
-
-
-echo ""
-echo ""
-echo "***********************************************************"
-echo "Vim will start then, then :so %"
-echo "When the script is done it'll compile the ruby extension"
-echo "***********************************************************"
-sleep 3
-vim /tmp/$vba
-sleep 3
-cd ~/.vim/ruby/command-t
-ruby extconf.rb
-make
