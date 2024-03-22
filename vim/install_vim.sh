137214b848084b8040867baa00f84d4c2df5e070
Youssef Chaker <youssef@bearandgiraffe.com>
2024-03-22 11:06:29 -0400

    Fixes typo

--- vim/install_vim.sh
+++ vim/install_vim.sh
@@ -6,13 +6,13 @@ rm -f ~/.vimrc ~/.gvimrc ~/.vimrc.bundles; rm -rf ~/.vim
 # Clone directly into ~/.vim
 git clone git@github:braintreeps/vim_dotfiles ~/.vim
 
+ln -sf "$PWD/.vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
+ln -sf "$PWD/.vimrc_local" "$HOME/.vimrc_local"
+
 ~/.vim/activate.sh
 
 # echo "Installing Seeing is Believing"
 # gem install seeing_is_believing
 
-ln -sf "$PWD/.vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
-ln -sf "$PWD/.vimrc_local" "$HOME/.vimrc_local"
-
-vim  +PlugInstall +PlugUpdate +PlugClear +qall
-nvim +PlugInstall +PlugUpdate +PlugClear +qall
+vim  +PlugInstall +PlugUpdate +PlugClean +qall
+nvim +PlugInstall +PlugUpdate +PlugClean +qall
